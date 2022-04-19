1f08f07efa1fc4f2ea5f7d676e462f2f65e5545b
apeli23 <apelibrian@gmail.com>
2022-04-20 01:30:47 +0300

    translateY

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,7 @@
 import { useEffect } from 'react';
 
 export default function Home() {
-
-
-  useEffect(() => {
-    animate()
-  }, [])
-
-
-  const animate = () => {
-    const banner = document.getElementsByClassName('banner')[0];
-    const blocks = document.getElementsByClassName('blocks');
-    console.log(blocks.length)
-
-    for (var i = 1; i < 400; i++) {
-      banner.innerHTML += "<div className='blocks'></div>";
-      
-    }
-  }
+ 
 
   return (
     <section>
